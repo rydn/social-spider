6d47c296ce6dd398350a715e0d02518c31b9a8e7
Ryan Dick <noxinoobin@gmail.com>
2013-05-04 23:23:25 +0200

    websocket and requirejs implemented

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express'),
     routes = require('./routes'),
     user = require('./routes/user'),
     http = require('http'),
     jobQueue = require('./lib/queue.js'),
     path = require('path');
-
 var app = express();
-
-
-app.configure(function() {
+app.configure(function () {
     app.set('port', process.env.PORT || 5599);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jshtml');
@@ -20,50 +16,50 @@ app.configure(function() {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.cookieParser('your secret here'));
+    app.use(express.cookieParser('m33p.or.m00p'));
     app.use(express.session());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
-
-app.configure('development', function() {
+app.configure('development', function () {
     app.use(express.errorHandler());
 });
-
 app.engine('jshtml', require('jshtml-express'));
 app.set('view engine', 'jshtml');
-
 ////////////
 //  Paths //
 ////////////
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/models/:modelName', function (req, res) {
+    var modelName = req.params.modelName;
+    console.log(modelName);
+});
 //  post auth get details
-app.post('/fb/init', function(req, res) {
+app.post('/fb/init', function (req, res) {
     if (req.body.access_token) {
         jobQueue.enqueue('getFriends', {
             access_token: req.body.access_token,
             userid: req.body.userid
-        }, function(err, job) {
+        }, function (err, job) {
             if (!err) {
                 // bind event handlers
-                job.on('complete', function() {
+                job.on('complete', function () {
                     console.log("Job: 'getFriends', is complete");
                     res.json();
-                }).on('failed', function() {
+                })
+                    .on('failed', function () {
                     console.log("Job: 'getFriends', failed");
-                }).on('progress', function(progress) {
+                })
+                    .on('progress', function (progress) {
                     process.stdout.write('\r  job #' + job.id + ' ' + progress + '% complete');
                 });
                 // process job
                 jobQueue.processQueue('getFriends');
-                
             } else res.json({
                 hasErr: true,
                 err: 'failed to enqueue job'
             });
         });
-
     } else {
         res.json({
             hasErr: true,
@@ -71,9 +67,23 @@ app.post('/fb/init', function(req, res) {
         })
     }
 });
-//  start server
-http.createServer(app).listen(app.get('port'), function() {
+//  start web server
+var webserver = http.createServer(app)
+    .listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
     jobQueue.startInterface('Social-Spider | Queue', 5598);
-    console.log('Kue interface listening on port 5598')
+    console.log('Kue interface listening on port 5598');
+});
+//  start websocket server
+var io = require('socket.io')
+    .listen(webserver);
+io.sockets.on('connection', function (socket) {
+    console.log('New connected client');
+    /*
+    socket events
+    */
+    //  on client disconnect
+    socket.on('disconnect', function () {
+        console.log('Client disconnect.');
+    });
 });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,52 +1,105 @@
-require(['javascripts/thirdparty/jquery/jquery.js', 'javascripts/thirdparty/sammy/lib/sammy.js'], function(jQuery, sammy) {
-    var login = function() {
-            FB.login(function(response) {
-                return response;
-            }, {
-                scope: 'email,user_likes,read_friendlists,read_insights,read_stream,friends_about_me,friends_activities,friends_birthday,friends_checkins,friends_education_history,friends_events,friends_games_activity,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_online_presence,friends_relationship_details,friends_relationships,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history'
-            });
-        }
+(function () {
+	require(['javascripts/thirdparty/jquery/jquery.js', 'javascripts/thirdparty/sammy/lib/sammy.js', 'socket.io/socket.io.js'], function (jQuery, Sammy, io) {
+		//	internal app instance for exporting vars out of namespace
+		var _app =  Object;
+		//	do facebook login
+		var login = function () {
+			FB.login(function (response) {
+				return response;
+			}, {
+				scope: 'email,user_likes,read_friendlists,read_insights,read_stream,friends_about_me,friends_activities,friends_birthday,friends_checkins,friends_education_history,friends_events,friends_games_activity,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_online_presence,friends_relationship_details,friends_relationships,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history'
+			});
+		};
+		//  postback to server with access_token to begin collection of friends lists
+		var getMutualFriends = function (response) {
+			$.ajax({
+				url: '/fb/init',
+				type: 'POST',
+				data: {
+					access_token: response.authResponse.accessToken,
+					userid: response.authResponse.userID
+				},
+				success: function (dataR) {
+					console.log(dataR);
+				}
+			});
+		};
+		//  executes 
 
-    var getMutualFriends = function(response) {
-            $.ajax({
-                url: '/fb/init',
-                type: 'POST',
-                data: {
-                    access_token: response.authResponse.accessToken,
-                    userid: response.authResponse.userID
-                },
-                success: function(dataR) {
-                    console.log(dataR);
-                }
-            });
-        }
-
-    function postLogin() {
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                $('.connection-status').html('Connected');
-
-            } else if (response.status === 'not_authorized') {
-                $('.connection-status').html('Not Authorized');
-                response = login();
-            } else {
-                $('.connection-status').html('Please login first');
-                response = login();
-            }
-        });
-    }
-
-    function pageInit($target) {
-        // define a new Sammy.Application bound to the $target element selector
-      Sammy($target.toString(), function() {
-
-        // define a 'get' route that will be triggered at '#/path'
-        this.get('#/', function() {
-          // this context is a Sammy.EventContext
-          this.$element() // $('#main')
-              .html('A new route!');
-        });
-      }).run();
-    }
-
-});
+		function postLogin() {
+			FB.getLoginStatus(function (response) {
+				if (response.status === 'connected') {
+					window.FBAuthorised = true;
+					$('.connection-status')
+						.html('Connected');
+				} else if (response.status === 'not_authorized') {
+					$('.connection-status')
+						.html('App not authorized');
+					response = login();
+				} else {
+					$('.connection-status')
+						.html('Please login first');
+					response = login();
+				}
+			});
+		}
+		//  initialise routes, is executed once FB-SDK is loaded
+		var pageInit = function (jQuerytarget) {
+			//	init websocket connection
+			_app.socket = io.connect('http://' + window.location.host);
+			// define a new Sammy.Application bound to the jQuerytarget element selector
+			var app = _app.app = Sammy(jQuerytarget.toString(), function () {
+				this.get('#/login', function () {
+					postLogin();
+					window.location = '#/home';
+				});
+				//  main page
+				this.get('#/home', function (context) {
+					if (window.FBAuthorised) {
+						//  connection to websocket is established
+						_app.socket.on('connect', function () {
+							context.element('#main')
+								.append('websocket connect');
+							context.load('/models/index')
+								.then(function (item) {
+								this.log(item);
+								return item[0];
+							});
+						});
+					} else {
+						this.log('Not Logged In');
+						window.location = '#/login';
+					}
+				});
+			});
+			if (!window.FBAuthorised) {
+				app.run('#/login');
+			}
+			return app;
+		};
+		//  async init
+		//  acts as a wrapper for main init
+		window.fbAsyncInit = function () {
+			// init the FB JS SDK
+			FB.init({
+				appId: '332579633512130',
+				status: true,
+				cookie: true,
+				xfbml: true
+			});
+			window.$app = pageInit('#main');
+		};
+		// Load the FB SDK asynchronously
+		(function (d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {
+				return;
+			}
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		return _app;
+	});
+})();
